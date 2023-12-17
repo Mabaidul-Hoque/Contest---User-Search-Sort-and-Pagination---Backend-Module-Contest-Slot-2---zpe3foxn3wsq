629640d89f8f369f005e24896fe148d7c04242da
Mabaidul-Hoque <mabaidulhoque95@gmail.com>
2023-12-17 17:58:02 +0530

    queryController updated

--- controllers/queryControllers.js
+++ controllers/queryControllers.js
@@ -10,6 +10,31 @@ const filterQueries = async (req, res) => {
     //3) Implement serching on 'first_name' and 'last_name'
     // Formulae to implementing pagination: (page - 1) * limit
     // For Sorting use    .sort('salary')
+
+    const sortField = req.query.sortBy || 'salary';
+    const sortOrder = req.query.sortOrder === 'disc' ? -1 : 1;
+    const sortQuery = { [ sortField ] : sortOrder};
+
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 5;
+    const skip = (page-1) * limit;
+
+    const searchQuery = {};
+    if(req.query.search) {
+      const searchRegex = new RegExp(req.query.search, 'i');
+      searchQuery.$or = [
+        {first_name: searchRegex},
+        {last_name: searchRegex},
+      ];
+    }
+
+    const users = await User.find(searchQuery)
+      .sort(sortQuery)
+      .skip(skip)
+      .limit(limit);
+
+    res.json(users);  
+
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Internal Server Error' });
